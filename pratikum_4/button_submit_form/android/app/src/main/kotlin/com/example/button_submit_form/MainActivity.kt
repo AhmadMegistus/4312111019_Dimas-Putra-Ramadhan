8d9b9d2a9ac41a5c519394e05e269ab6ef9334a3
AhmadMegistus <dimasputraramadhan2002@gmail.com>
2023-09-17 18:49:16 +0700

    a

---
+++ pratikum_4/button_submit_form/android/app/src/main/kotlin/com/example/button_submit_form/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.button_submit_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pratikum_4/text_from_field_validasi/android/app/src/main/kotlin/com/example/text_from_field_validasi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_from_field_validasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
